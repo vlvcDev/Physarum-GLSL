ea098a9c017c45e6af0234a982f9f79de913ff10
vlvcDev <vlvc.dev@gmail.com>
2023-11-20 03:58:39 -0700

    Add project files.

---
+++ src/ofApp.cpp
@@ -0,0 +1,181 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	// Load the shader
+
+	ofBackground(0, 0, 0);
+
+	windowWidth = 1920;
+	windowHeight = 1200;
+
+	numSlimes = 200000; // The number of slime objects
+
+	slimes.resize(numSlimes);
+
+	if (!slimeShader.loadCompute("slimeShader.glsl")) {
+		ofLogError("Shader Program") << "Could not load diffuse shader";
+	}
+	else {
+		ofLogNotice("Shader Program") << "Compute shader loaded successfully";
+	}
+
+
+	if (!diffuseShader.loadCompute("diffuseShader.glsl")) { // Assuming shaders are in "shaders" folder
+		ofLogError("Shader Program") << "Could not load diffuse shader";
+	}
+	else {
+		ofLogNotice("Shader Program") << "Diffuse shader loaded successfully";
+	}
+
+	// Allocate the texture
+	// Initialize the texture that will be used to store the trail map
+	trailMapTexture.allocate(windowWidth, windowHeight, GL_RGBA32F); // Assuming 'width' and 'height' are defined
+	trailMapTexture.bindAsImage(1, GL_READ_WRITE);
+
+	// Initialize uniform variables
+	moveSpeed = 2.0f; // The speed at which slimes move
+	sensorSize = 1;
+	sensorAngleOffset = 0.098f;
+	sensorOffsetDistance = 25.0f;
+	turnSpeed = 0.001f;
+	
+	deltaTime = 1.0f / 60;
+
+	diffuseWeight = 10.0f;
+	decayRate = 0.000022f;
+
+	//for (int i = 0; i < numSlimes; ++i) {
+	//	// Set initial position and angle
+	//	slimes[i].position = glm::vec2(windowWidth/2, windowHeight/2);
+	//	slimes[i].angle = -ofRandom(2 * 3.1415); // Random angle between 0 and 2*PI
+	//}
+
+	// Parameters for the circle
+	glm::vec2 circleCenter = glm::vec2(windowWidth / 2, windowHeight / 2);
+	float circleRadius = 400.0f; // Set the radius of the circle
+
+	for (int i = 0; i < numSlimes; ++i) {
+		// Generate a random angle and radius
+		float angle = ofRandom(TWO_PI);
+		float radius = ofRandom(circleRadius); // Random radius within the circle
+
+		// Position each slime inside the circle
+		slimes[i].position = glm::vec2(
+			circleCenter.x + radius * cos(angle),
+			circleCenter.y + radius * sin(angle)
+		);
+
+		// Set the angle of each slime to point towards the center of the circle
+		slimes[i].angle = atan2(circleCenter.y - slimes[i].position.y, circleCenter.x - slimes[i].position.x);
+	}
+
+	// Upload slimes data to GPU
+	slimesBuffer.allocate(slimes, GL_STATIC_DRAW); // Use GL_DYNAMIC_DRAW if the data will change often
+	// ...
+
+
+	std::cout << "Size of Slime: " << sizeof(Slime) << " bytes" << std::endl;
+	std::cout << "Offset of position: " << offsetof(Slime, position) << " bytes" << std::endl;
+	std::cout << "Offset of angle: " << offsetof(Slime, angle) << " bytes" << std::endl;
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	deltaTime = 1.0f / ofGetLastFrameTime();
+
+	// Dispatch shader for slime updates
+	slimeShader.begin();
+	slimeShader.setUniform1i("numSlimes", numSlimes);
+	slimeShader.setUniform1f("deltaTime", deltaTime);
+	slimeShader.setUniform1f("moveSpeed", moveSpeed);
+	slimeShader.setUniform1i("width", windowWidth);
+	slimeShader.setUniform1i("height", windowHeight);
+	slimeShader.setUniform1f("diffuseWeight", diffuseWeight);
+	slimeShader.setUniform1f("decayRate", decayRate);
+	slimeShader.setUniform1i("sensorSize", sensorSize);
+	slimeShader.setUniform1f("sensorAngleOffset", sensorAngleOffset);
+	slimeShader.setUniform1f("sensorOffsetDistance", sensorOffsetDistance);
+	slimeShader.setUniform1f("turnSpeed", turnSpeed);
+	slimesBuffer.bindBase(GL_SHADER_STORAGE_BUFFER, 0);
+	int numGroupsSlime = (numSlimes + 8 - 1) / 8;
+	slimeShader.dispatchCompute(numGroupsSlime, 1, 1);
+	slimesBuffer.unbindBase(GL_SHADER_STORAGE_BUFFER, 0);
+	
+
+	// Additional dispatch for texture diffusion
+	diffuseShader.begin();
+	diffuseShader.setUniform1i("width", windowWidth);
+	diffuseShader.setUniform1i("height", windowHeight);
+	diffuseShader.setUniform1f("deltaTime", deltaTime);
+	diffuseShader.setUniform1f("diffuseWeight", diffuseWeight);
+	diffuseShader.setUniform1f("decayRate", decayRate);
+	trailMapTexture.bindAsImage(1, GL_READ_WRITE); // Or whatever binding point is used
+	int numGroupsX = (windowWidth + 8 - 1) / 8;
+	int numGroupsY = (windowHeight + 8 - 1) / 8;
+	diffuseShader.dispatchCompute(numGroupsX, numGroupsY, 1);
+	diffuseShader.end();
+	slimeShader.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	trailMapTexture.draw(0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
